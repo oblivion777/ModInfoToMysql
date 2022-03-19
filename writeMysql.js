b00e9ef7e433fa9cae3ce6e4efde7a1a9079b4f1
oblivion777 <dyangel@foxmail.com>
2022-03-19 14:23:41 +0800

    预编译防SQL注入

--- writeMysql.js
+++ writeMysql.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var fs = require('fs');
 
 exports.writeMysql = function (page = "test", ip = "0.0.0.0") {
@@ -10,24 +10,24 @@ exports.writeMysql = function (page = "test", ip = "0.0.0.0") {
 
 function toPV(page = "test") {
     //connectMysql.query(`INSERT INTO pv (page) VALUE ("test2")`);
-    connectMysql.query(`SELECT page,count FROM pv WHERE page="${page}"`, function (err, result) {
+    connectMysql.execute(`SELECT page,count FROM pv WHERE page= ? `, [page], function (err, result) {
         //console.log(result);
         connectMysql.query(`UPDATE pv SET count=count+1 WHERE page="total_page_views"`);
         if (result[0] == undefined) {
-            return connectMysql.query(`INSERT INTO pv (page) VALUE ("${page}")`);
+            return connectMysql.execute(`INSERT INTO pv (page) VALUE (?)`, [page]);
         } else {
-            return connectMysql.query(`UPDATE pv SET count=count+1 WHERE page="${page}"`);
+            return connectMysql.execute(`UPDATE pv SET count=count+1 WHERE page= ? `, [page]);
         };
     });
 }
 
 function toIP(ip) {
-    connectMysql.query(`SELECT ip,count FROM ip WHERE ip="${ip}"`, function (err, result) {
+    connectMysql.query(`SELECT ip,count FROM ip WHERE ip= ? `,[ip], function (err, result) {
         //console.log(result);
         if (result[0] == undefined) {
-            return connectMysql.query(`INSERT INTO ip (ip) VALUE ("${ip}")`);
+            return connectMysql.query(`INSERT INTO ip (ip) VALUE (?)`,[ip]);
         } else {
-            return connectMysql.query(`UPDATE ip SET count=count+1 WHERE ip="${ip}"`);
+            return connectMysql.query(`UPDATE ip SET count=count+1 WHERE ip= ? `,[ip]);
         };
     });
 }

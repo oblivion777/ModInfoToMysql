992e34b071f57bef1acfac41520958d63bf46671
oblivion777 <dyangel@foxmail.com>
2022-03-23 19:27:19 +0800

    使用MySQL连接池

--- writeMysql.js
+++ writeMysql.js
@@ -8,31 +8,34 @@ exports.writeMysql = function (page = "test", ip = "0.0.0.0") {
     //connectMysql.end();
 };
 
+//写入PV数据
 function toPV(page = "test") {
-    //connectMysql.query(`INSERT INTO pv (page) VALUE ("test2")`);
-    connectMysql.execute(`SELECT page,count FROM pv WHERE page= ? `, [page], function (err, result) {
+    pool.execute(`SELECT page,count FROM pv WHERE page= ? `, [page], function (err, result) {
         //console.log(result);
-        connectMysql.query(`UPDATE pv SET count=count+1 WHERE page="total_page_views"`);
+        pool.query(`UPDATE pv SET count=count+1 WHERE page="total_page_views"`);
         if (result[0] == undefined) {
-            return connectMysql.execute(`INSERT INTO pv (page) VALUE (?)`, [page]);
+            return pool.execute(`INSERT INTO pv (page) VALUE (?)`, [page]);
         } else {
-            return connectMysql.execute(`UPDATE pv SET count=count+1 WHERE page= ? `, [page]);
+            return pool.execute(`UPDATE pv SET count=count+1 WHERE page= ? `, [page]);
         };
     });
 }
 
+
+//写入UV数据
 function toIP(ip) {
-    connectMysql.query(`SELECT ip,count FROM ip WHERE ip= ? `,[ip], function (err, result) {
+    pool.query(`SELECT ip,count FROM ip WHERE ip= ? `, [ip], function (err, result) {
         //console.log(result);
         if (result[0] == undefined) {
-            return connectMysql.query(`INSERT INTO ip (ip) VALUE (?)`,[ip]);
+            return pool.query(`INSERT INTO ip (ip) VALUE (?)`, [ip]);
         } else {
-            return connectMysql.query(`UPDATE ip SET count=count+1 WHERE ip= ? `,[ip]);
+            return pool.query(`UPDATE ip SET count=count+1 WHERE ip= ? `, [ip]);
         };
     });
 }
 
 const mysqlConfig = JSON.parse(fs.readFileSync("./ssl-key/mysql-key.json"));
 const connectMysql = mysql.createConnection(mysqlConfig);
+const pool = mysql.createPool(mysqlConfig);
 
 //connectMysql.query(`SELECT page,count FROM pv`, function (err, result) { console.log(result); })

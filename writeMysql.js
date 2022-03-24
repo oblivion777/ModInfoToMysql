8bcb854ed06471a8740ef306eb72b606db922d63
oblivion777 <dyangel@foxmail.com>
2022-03-24 19:58:00 +0800

    更新字段名

--- writeMysql.js
+++ writeMysql.js
@@ -10,13 +10,13 @@ exports.writeMysql = function (page = "test", ip = "0.0.0.0") {
 
 //写入PV数据
 function toPV(page = "test") {
-    pool.execute(`SELECT page,count FROM pv WHERE page= ? `, [page], function (err, result) {
+    pool.execute(`SELECT page,views FROM pv WHERE page= ? `, [page], function (err, result) {
         //console.log(result);
-        pool.query(`UPDATE pv SET count=count+1 WHERE page="total_page_views"`);
+        pool.query(`UPDATE pv SET views=views+1 WHERE page="total_page_views"`);
         if (result[0] == undefined) {
             return pool.execute(`INSERT INTO pv (page) VALUE (?)`, [page]);
         } else {
-            return pool.execute(`UPDATE pv SET count=count+1 WHERE page= ? `, [page]);
+            return pool.execute(`UPDATE pv SET views=views+1 WHERE page= ? `, [page]);
         };
     });
 }
@@ -24,12 +24,12 @@ function toPV(page = "test") {
 
 //写入UV数据
 function toIP(ip) {
-    pool.query(`SELECT ip,count FROM ip WHERE ip= ? `, [ip], function (err, result) {
+    pool.query(`SELECT ip,views FROM ip WHERE ip= ? `, [ip], function (err, result) {
         //console.log(result);
         if (result[0] == undefined) {
             return pool.query(`INSERT INTO ip (ip) VALUE (?)`, [ip]);
         } else {
-            return pool.query(`UPDATE ip SET count=count+1 WHERE ip= ? `, [ip]);
+            return pool.query(`UPDATE ip SET views=views+1 WHERE ip= ? `, [ip]);
         };
     });
 }
@@ -38,4 +38,4 @@ const mysqlConfig = JSON.parse(fs.readFileSync("./ssl-key/mysql-key.json"));
 const connectMysql = mysql.createConnection(mysqlConfig);
 const pool = mysql.createPool(mysqlConfig);
 
-//connectMysql.query(`SELECT page,count FROM pv`, function (err, result) { console.log(result); })
+//connectMysql.query(`SELECT page,views FROM pv`, function (err, result) { console.log(result); })
